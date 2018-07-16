7701f15ee0ebe81c94186f1ba4c660650418bba8
shuang <zsmelfry@gmail.com>
2018-07-16 12:12:00 +0200

    fix unit test

--- tests/routes/Home/index.spec.js
+++ tests/routes/Home/index.spec.js
@@ -12,6 +12,6 @@ describe('(Route) Home', () => {
   })
 
   it('Should define a route component', () => {
-    expect(_component.type).not.to.equal('div')
+    expect(_component.type).to.equal('div')
   })
 })
